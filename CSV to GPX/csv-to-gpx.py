fdffd1c13f337fa3e5baa561e7abcde48a0299d3
Shlomo <buoyant.puzzle0k@icloud.com>
2025-05-15 04:02:32 -0400

    refactoring
    Added namedtuple for points, moved stuff out of main process to
    variables and a function

--- CSV to GPX/csv-to-gpx.py
+++ CSV to GPX/csv-to-gpx.py
@@ -1,53 +1,58 @@
 import csv
+from collections import namedtuple
 from datetime import datetime
 from zoneinfo import ZoneInfo
+
+Point = namedtuple('Point', 'date time lat lon ele')
 
 eastern = ZoneInfo("America/New_York")
 utc = ZoneInfo("UTC")
 
-
-with open('out.gpx', 'a') as outfile:
-   outfile.write('''\
+preamble = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <gpx version="1.1"
      creator="csv-to-gpx"
      xmlns="http://www.topografix.com/GPX/1/1"
      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
      xsi:schemaLocation="
-       http://www.topografix.com/GPX/1/1
-       https://www.topografix.com/GPX/1/1/gpx.xsd">
+        http://www.topografix.com/GPX/1/1
+        https://www.topografix.com/GPX/1/1/gpx.xsd">
 
   <trk>
-    <name>Example Track</name>
-''')
+    <name>csv to gpx</name>'''
+segstart = '''
+    <trkseg>'''
+segend = '''
+    </trkseg>'''
+postamble = '''
+  </trk>
 
-   outfile.write('''\
-    <trkseg>
-''')
+</gpx>\n'''
+
+
+def gpxpoint(point):
+   utc_dt = (datetime.fromisoformat(f'{point.date}T{point.time}')
+                     .replace(tzinfo=eastern)
+                     .astimezone(utc)
+                     .isoformat()
+                     .replace('+00:00', 'Z'))
+   return f'''
+      <trkpt lat="{point.lat}" lon="{point.lon}">
+        <ele>{point.ele}</ele>
+        <time>{utc_dt}</time>
+      </trkpt>'''
+
+
+with open('out.gpx', 'a') as outfile:
+   outfile.write(preamble)
+   outfile.write(segstart)
 
    with open('raw.csv', newline='') as infile:
       reader = csv.reader(infile, skipinitialspace=True)
 
-      for date, time, lat, lon, ele in reader:
-         utc_dt = (datetime.fromisoformat(f'{date}T{time}')
-                           .replace(tzinfo=eastern)
-                           .astimezone(utc))
-         utc_dt_str = utc_dt.isoformat().replace('+00:00', 'Z')
+      for point in map(Point._make, reader):
+         outfile.write(gpxpoint(point))
 
-         outfile.write(f'''\
-      <trkpt lat="{lat}" lon="{lon}">
-        <ele>{ele}</ele>
-        <time>{utc_dt_str}</time>
-      </trkpt>
-''')
-
-   outfile.write('''\
-    </trkseg>
-''')
-
-   outfile.write('''\
-  </trk>
-
-</gpx>
-''')
+   outfile.write(segend)
+   outfile.write(postamble)
 
